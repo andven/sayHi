9b946e96e69b25b69f4c5799932af7d156383b19
datasick <andrea@kome.it>
2018-11-18 23:57:29 +0100

    Adding a second comment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-//        Adding a comment
+        //        Adding a comment
+        //        Adding a second comment
         System.out.println("Hello World!");
     }
 }
